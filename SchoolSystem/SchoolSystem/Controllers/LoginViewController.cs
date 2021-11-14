757c599f059074b6b9b5b4ebdcc9cd3c11dfcb3e
AndyPeep <78352932+AndyPeep@users.noreply.github.com>
2021-11-14 21:12:12 +0200

    Add files via upload

---
+++ SchoolSystem/SchoolSystem/Controllers/LoginViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolSystem.Controllers
+{
+    public class LoginViewController
+    {
+        public void AddUser(string username, string password)
+        {
+            MessageBox.Show("User " + username + " was added to the system!");
+        }
+    }
+}
---
+++ SchoolSystem/SchoolSystem/Views/LoginView.cs
@@ -0,0 +1,36 @@
+﻿using SchoolSystem.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolSystem.Views
+{
+    public partial class LoginView : Form
+    {
+        LoginViewController loginViewController = new LoginViewController();
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            loginViewController.AddUser(txtUsername.Text, txtPassword.Text);
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
